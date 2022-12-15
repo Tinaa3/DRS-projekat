8b05ab2247400761246a3be6f635da6e9f876a46
nikola20 <nikola.j00@gmail.com>
2022-12-15 16:23:01 +0100

    User class

---
+++ Classes/User.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+#db.create_all()
+
+class User(db.Model):
+    name = db.Column(db.String(length=30), nullable = False)
+    lastname = db.Column(db.String(length=30), nullable = False)
+    address = db.Column(db.String(length=30), nullable = False)
+    city = db.Column(db.String(length=30), nullable = False)
+    country = db.Column(db.String(length=30), nullable = False)
+    phoneNumber = db.Column(db.String(length=30), nullable = False)
+    email = db.Column(db.String(length=30), nullable = False)
+    password = db.Column(db.String(length=30), nullable = False)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.lastname}', {self.address}, '{self.city}', '{self.country}', '{self.phoneNumber}', '{self.email}', '{self.password}')"
+
+    def __init__(self, name, lastname, address, city, country, phoneNumber, email, password):
+        self.name = name
+        self.lastname = lastname
+        self.address = address
+        self.city = city
+        self.country = country
+        self.phoneNumber = phoneNumber
+        self.email = email
+        self.password = password
+
+def read(emailAddress):
+    return User.query.filter_by(email=emailAddress).first()
+
+def add(newUser, flag):
+    if flag:
+        db.session.add(newUser)
+    db.session.commit()
+
+def delete(emailAddress):
+    User.query.filter_by(email=emailAddress).delete()
+    return
