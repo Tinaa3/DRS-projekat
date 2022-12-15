981ca2f1c1a5e995373152f6dcdec43bc9175247
nikola20 <nikola.j00@gmail.com>
2022-12-15 14:51:38 +0100

    inital files

--- Engine/Engine.py
+++ Engine/Engine.py
@@ -1,1 +1,11 @@
-import json
+from flask import Flask, render_template, app
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("///UI/templates/home.html")
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
