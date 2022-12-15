bb23489b8a2aba17566092a07cbba14dd240e1a1
Bojana Živanić <zivanic.pr122.2019@uns.ac.rs>
2022-12-15 18:26:54 +0100

    db

--- Classes/User.py
+++ Classes/User.py
@@ -1,12 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from UI import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-#db.create_all()
 
 class User(db.Model):
     name = db.Column(db.String(length=30), nullable = False)
