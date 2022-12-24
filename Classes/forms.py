c744a8101201b36724268b043ff502a417dfea9c
nikola20 <nikola.j00@gmail.com>
2022-12-24 18:47:08 +0100

    registration form

--- Classes/forms.py
+++ Classes/forms.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, IntegerField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError, Regexp
 from Classes.User import User
 
 class RegisterForm(FlaskForm):
-    name=StringField(label='Name')
-    lastname=StringField(label='Lastname')
-    address=StringField(label='Address')
-    city=StringField(label='City')
-    country = StringField(label='Country')
-    phoneNumber = IntegerField(label='Phone Number')
-    email=EmailField(label='Email Address')
-    password1 = PasswordField(label='password1')
-    password2 = PasswordField(label='password2')
+    name=StringField(label='Name', validators=[Length(min=2, max=30), DataRequired()])
+    lastname=StringField(label='Lastname', validators=[Length(min=2, max=30), DataRequired()])
+    address=StringField(label='Address', validators=[DataRequired()])
+    city=StringField(label='City', validators=[DataRequired()])
+    country = StringField(label='Country', validators=[DataRequired()])
+    phoneNumber = IntegerField(label='Phone Number', validators=[DataRequired()])
+    email=EmailField(label='Email Address', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='password1', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='password2', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
 class LoginForm(FlaskForm):
--- UI/Routes.py
+++ UI/Routes.py
@@ -1,16 +1,37 @@
-from UI import app
+from UI import app, db
 from flask import render_template, redirect, url_for, flash, request
 from Classes.forms import RegisterForm, LoginForm
 from Classes.User import User
 from flask_login import login_user, logout_user, login_required, current_user
 
 @app.route('/')
-@app.route('/home', methods=['GET', 'SET'])
+@app.route('/home', methods=['GET', 'POST'])
 def home_page():
     form = LoginForm()
     return render_template('home.html', form=form)
 
-@app.route('/register', methods=['GET', 'SET'])
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(name=form.name.data,
+                                lastname=form.lastname.data,
+                                address=form.address.data,
+                                city=form.city.data,
+                                country=form.country.data,
+                                phoneNumber=form.phoneNumber.data,
+                                email=form.email.data,
+                                password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        login_user(user_to_create)
+        flash(f'Account created successufuly! You are now logged in as {user_to_create.name}', category='success')
+        return redirect(url_for('profile_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
     return render_template('register.html', form=form)
+
+@app.route('/profile')
+def profile_page():
+    return render_template('profile.html')
