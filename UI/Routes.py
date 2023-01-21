9093e244ec6482e365bb852901d8919015d0c98e
Bojana Živanić <zivanic.pr122.2019@uns.ac.rs>
2023-01-21 16:42:22 +0100

    Validation

--- UI/Routes.py
+++ UI/Routes.py
@@ -41,7 +41,11 @@ def login_page():
     #else: # if the request is POST the we check if the user exist 
           # and with te right password
     email = request.form.get('email')
+    if(email == "" or email.isspace()):
+        return redirect(url_for('login_page'))
     password = request.form.get('password')
+    if(password == "" or password.isspace()):
+        return redirect(url_for('login_page'))
     
     user = User.query.filter_by(email=email).first()
     
@@ -61,6 +65,16 @@ def login_page():
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
+        if (form.name.data == "" or form.name.data.isspace()
+            or form.lastname.data == "" or form.lastname.data.isspace()
+            or form.address.data == "" or form.address.data.isspace()
+            or form.city.data == "" or form.city.data.isspace()
+            or form.country.data == "" or form.country.data.isspace()
+            or form.phoneNumber.data == "" or form.phoneNumber.data.isspace()
+            or form.email.data == "" or form.email.data.isspace()
+            or form.password1.data == "" or form.password1.data.isspace()
+            or form.password2.data == "" or form.password2.data.isspace()):
+            return redirect(url_for('register_page'))
         user_to_create = User(name=form.name.data,
                                 lastname=form.lastname.data,
                                 address=form.address.data,
@@ -85,6 +99,8 @@ def profile_page():
         #table of transactions - 6.
     if request.method=='POST':
         sold_transaction_id = request.form.get('sold_transaction')
+        if(sold_transaction_id == None or not sold_transaction_id.isnumeric()):
+            return redirect(url_for('profile_page'))
         result_queue = Queue()
         p = Process(target=sell_coin, args=(sold_transaction_id, current_user.id, result_queue))
         p.start()   
@@ -136,10 +152,15 @@ def card_page():
     num_cards = Card.query.filter_by(owner_id=user_id).count()
     if request.method == 'POST':
         if num_cards >= 1:
-            
             flash(f"Only one credit card can be added")
             return redirect(url_for('card_page'))
         else:
+            if(form.name.data == "" or form.name.data.isspace()
+                or form.cardnum.data == None or not form.cardnum.data.isnumeric()
+                or form.expdate.data == "" or form.name.data.isspace()
+                or form.seccode.data == None or not form.seccode.data.isnumeric()
+                or form.amount.data == None or not form.amount.data.isnumeric()):
+                return redirect(url_for('card_page'))
             card = Card(name=form.name.data,
                         cardNum=form.cardnum.data,
                         expDate=form.expdate.data,
@@ -159,19 +180,23 @@ def edit_page():
     if form.is_submitted() is False:
         return render_template('editProfile.html', form=form)
     userUpdate = User.query.filter_by(id=current_user.id).first()
-    if(form.name.data != ""):
+    if(form.name.data != "" and not form.name.data.isspace()):
         userUpdate.name = form.name.data
-    if(form.lastname.data != ""):
+    if(form.lastname.data != "" and not form.lastname.data.isspace()):
         userUpdate.lastname = form.lastname.data
-    if(form.address.data != ""):
+    if(form.address.data != "" and not form.address.data.isspace()):
         userUpdate.address = form.address.data
-    if(form.city.data != ""):
+    if(form.city.data != "" and not form.city.data.isspace()):
         userUpdate.city = form.city.data
-    if(form.country.data != ""):
+    if(form.country.data != "" and not form.country.data.isspace()):
         userUpdate.country = form.country.data
-    if(form.country.data != ""):
+    if(form.phoneNumber.data != "" and not form.phoneNumber.data.isspace()):
         userUpdate.phoneNumber = form.phoneNumber.data
-    if(form.password1.data == form.password2.data and form.password1.data != "" and form.password2.data != ""):
+    if(form.password1.data == form.password2.data 
+        and form.password1.data != ""  
+        and not form.password1.data.isspace() 
+        and form.password2.data != ""
+        and not form.password2.data.isspace()):
         userUpdate.password_hash = form.password1.data
     db.session.commit()
     return redirect(url_for('profile_page'))
